97a75f48c51cde2e16130b83803d282feedded84
Varun JP <92174003+Varun-JP@users.noreply.github.com>
2026-03-04 01:16:29 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (64.06%) - LeetSync

---
+++ 61-rotate-list/rotate-list.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        int n =1;
+        if (!head || !head->next) return head;
+        ListNode* new_tail= head ;
+        ListNode* tail = head ;
+        ListNode* new_head = head;
+        while(tail -> next ){
+            n++;
+            tail = tail -> next; 
+        }
+        k = k% n ;
+        if (k==0) return head;
+
+        for(int i=1; i< n-k ;i++){
+            new_tail = new_tail-> next;  //  till k-1 element 
+        }
+        new_head = new_tail -> next; // next of the new tail to wrap around
+        tail-> next = head; //wrap around since orignal is breaking at 
+        new_tail-> next = nullptr;
+        return new_head;
+    }
+};
