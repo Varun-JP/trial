7e8b7e86c2771447a9e445e939ab213e01da9a32
Varun JP <92174003+Varun-JP@users.noreply.github.com>
2026-03-04 22:56:26 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (95.96%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -14,22 +14,27 @@ public:
         if (!head || k == 1) return head;
         ListNode* curr = head;
         ListNode* prevGroupEnd = nullptr;
+        
         while (curr != nullptr){
-            ListNode* kth = curr;
+            ListNode* kth = curr; 
+            //start of the current group we are about to reverse
             for (int i = 1; i < k && kth != nullptr; i++) {
                  kth = kth->next;
             }
             if (kth == nullptr) break;
+            //if nullptr then same as before so no change for that 
             ListNode* nextGroupStart = kth->next;
             ListNode* prev = nextGroupStart;
+             //end point just as prev = nullptr for regular reverse
             ListNode* node = curr;
-
+            //node goes from the first of the group to the last . 
             while (node != nextGroupStart) {
-                 ListNode* temp = node->next;
+                ListNode* temp = node->next;
                 node->next = prev;
                 prev = node;
-                 node = temp;
+                node = temp;
                 }  
+            //we are reversing our very first group then this
             if (prevGroupEnd == nullptr)
                  head = kth;
             else
