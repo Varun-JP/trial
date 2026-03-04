cb6fc0179a8fda5203d80e099fc9bd67b36a286e
Varun JP <92174003+Varun-JP@users.noreply.github.com>
2026-03-04 23:30:29 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (7.02%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -1,37 +1,47 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode* prev;
+        if (!head || k == 1) return head;
         ListNode* curr = head;
-        ListNode* newStart;
-        ListNode* prevEnd = nullptr;
-        ListNode* kth;
-        ListNode* node;
-        while(curr!=nullptr){
-            kth = curr;
-            for(int i=1; i< k && kth!=nullptr ; i++){
-                kth = kth->next;
+        ListNode* prevGroupEnd = nullptr;
+        
+        while (curr != nullptr){
+            ListNode* kth = curr; 
+            //start of the current group we are about to reverse
+            for (int i = 1; i < k && kth != nullptr; i++) {
+                 kth = kth->next;
             }
-            if(kth == 0) break;
-            newStart = kth->next;
-            prev = newStart;
-            node = curr;
-            while(node!= newStart){
+            if (kth == nullptr) break;
+            //if nullptr then same as before so no change for that 
+            ListNode* nextGroupStart = kth->next;
+            ListNode* prev = nextGroupStart;
+             //end point just as prev = nullptr for regular reverse
+            ListNode* node = curr;
+            //node goes from the first of the group to the last . 
+            while (node != nextGroupStart) {
                 ListNode* temp = node->next;
                 node->next = prev;
                 prev = node;
                 node = temp;
-            }
-            if(prevEnd == nullptr){
-                head = kth;
-            }
-            else{
-                prevEnd->next = kth;
-            }
-            prevEnd = curr;
-            curr = newStart;
+                }  
+            //we are reversing our very first group then this
+            if (prevGroupEnd == nullptr)
+                 head = kth;
+            else
+                 prevGroupEnd->next = kth;   
 
+            prevGroupEnd = curr;
+            curr = nextGroupStart;           
         }
         return head;
     }
