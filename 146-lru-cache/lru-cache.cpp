1ee06d456d53d0be1cb239849d6d21f13311510c
Varun JP <92174003+Varun-JP@users.noreply.github.com>
2026-03-07 23:17:36 +0530

    Time: 41 ms (98.48%) | Memory: 168.6 MB (99.93%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,72 +1,118 @@
-class LRUCache {
-private:
-    struct Node {
-        int key, value;
-        Node* prev;
-        Node* next;
-        Node(int k, int v) : key(k), value(v), prev(nullptr), next(nullptr) {}
-    };
+struct Node;
 
-    int cap;
-    unordered_map<int, Node*> mp;
-    Node* head;
-    Node* tail;
+struct NodePtr {
+    uint16_t ptr = 0;
+    constexpr NodePtr() = default;
+    template <typename T>
+    constexpr NodePtr(T x) : ptr{(uint16_t)x} {}
+    Node* operator->();
+    operator bool() const { return ptr; }
+};
 
-    void addNode(Node* node) {
-        node->next = head->next;
-        node->prev = head;
-        head->next->prev = node;
-        head->next = node;
+
+struct Node {
+    uint16_t key = 0, value = 0;
+    NodePtr prev = 0, next = 0;
+    Node() = default;
+    Node(uint16_t key, uint16_t value) : key{key}, value{value} {};
+};
+alignas(64) constinit Node node_alloc[3000]{};
+
+Node* NodePtr::operator->() {
+    return &node_alloc[ptr - 1];
+}
+
+constinit NodePtr fast_map[(int)1e4 + 1]{};
+struct List {
+    NodePtr head = 0, tail = 0;
+
+    NodePtr& next_prev(NodePtr node) {
+        return node->next ? node->next->prev : tail;
     }
 
-    void removeNode(Node* node) {
-        node->prev->next = node->next;
-        node->next->prev = node->prev;
+    NodePtr& prev_next(NodePtr node) {
+        return node->prev ? node->prev->next : head;
     }
 
-    void moveToFront(Node* node) {
-        removeNode(node);
-        addNode(node);
+    void put(NodePtr node) {
+        node->next = head;
+        next_prev(node) = node;
+        head = node;
     }
 
-    void removeLRU() {
-        Node* lru = tail->prev;
-        removeNode(lru);
-        mp.erase(lru->key);
-        delete lru;
+    NodePtr pop(NodePtr node = 0) {
+        node = node ? node : tail;
+
+        next_prev(node) = node->prev;
+        prev_next(node) = node->next;
+
+        node->prev = node->next = 0;
+        return node;
     }
 
-public:
-    LRUCache(int capacity) {
-        cap = capacity;
-        head = new Node(0, 0);
-        tail = new Node(0, 0);
-        head->next = tail;
-        tail->prev = head;
-    }
-
-    int get(int key) {
-        if (mp.find(key) == mp.end())
-            return -1;
-
-        Node* node = mp[key];
-        moveToFront(node);
-        return node->value;
-    }
-
-    void put(int key, int value) {
-        if (mp.find(key) != mp.end()) {
-            Node* node = mp[key];
-            node->value = value;
-            moveToFront(node);
-        } else {
-            if (mp.size() == cap) {
-                removeLRU();
-            }
-            Node* node = new Node(key, value);
-            mp[key] = node;
-            addNode(node);
+    ~List() {
+        while (head) {
+            NodePtr next = head->next;
+            fast_map[head->key] = 0;
+            head = next;
         }
     }
 };
-auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
+
+uint16_t node_pos = 0;
+NodePtr new_node(uint16_t key, uint16_t value) {
+    Node& node = node_alloc[node_pos++];
+    node = Node(key, value);
+    return node_pos;
+}
+
+class LRUCache {
+    uint16_t capacity_ = 0;
+    List cache_;
+
+    NodePtr tryGet(uint16_t key) {
+        if (auto it = fast_map[key]; it != 0) {
+            cache_.put(cache_.pop(it));
+            return it;
+        }
+        return 0;
+    }
+
+public:
+    LRUCache(int capacity) : capacity_{(uint16_t)capacity} { node_pos = 0; }
+    
+    int get(int key) {
+        if (NodePtr node = tryGet(key); node != 0) {
+            return node->value;
+        }
+        return -1;
+    }
+
+    
+    void put(int key, int value) {
+        if (NodePtr node = tryGet(key); node != 0) {
+            node->value = value;
+            return;
+        }
+
+        NodePtr node = 0;
+        if (capacity_ == 0) {
+            node = cache_.pop();
+            fast_map[node->key] = 0;
+            node->key = key;
+            node->value = value;
+        } else {
+            --capacity_;
+            node = new_node(key, value);
+        }
+        fast_map[key] = node;
+        cache_.put(node);
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * uint16_t param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
