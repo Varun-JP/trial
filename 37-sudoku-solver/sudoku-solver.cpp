a048429476339b8458ec5363816f2eb3981c4b76
Varun JP <92174003+Varun-JP@users.noreply.github.com>
2026-03-09 23:28:49 +0530

    Time: 27 ms (98.01%) | Memory: 8.7 MB (69.59%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,41 +1,65 @@
 class Solution {
 public:
-    bool row[10][10];
-    bool col[10][10];
-    bool box[10][10];
-    bool solve(vector<vector<char>> &board ){
-        for(int r = 0; r< 9 ; r++){
-            for(int c =0 ; c < 9 ; c++){
-                if(board[r][c] == '.'){
-                    int b = (r/3)*3 + (c/3);
-                    for(int d =1 ; d<= 9 ;d++){
-                        if(row[r][d] || col[c][d] || box[b][d])   continue;
-                        board[r][c] = '0' + d ;
-                        row[r][d] = 1; 
-                        col[c][d] = 1;
-                        box[b][d] =1;
-                        if(solve(board)) return true;
-                        row[r][d] = 0; 
-                        col[c][d] = 0;
-                        box[b][d] =0;
-                        board[r][c] = '.' ;            
-                    }
-                    return false;
+    int row[9] = {0};
+    int col[9] = {0};
+    int box[9] = {0};
+    vector<pair<int,int>> empty;
+
+    bool solve(vector<vector<char>>& board, int idx){
+
+        if(idx == empty.size()) return true;
+
+        auto [r,c] = empty[idx];
+        int b = (r/3)*3 + c/3;
+
+        int mask = ~(row[r] | col[c] | box[b]) & 0x1FF;
+
+        while(mask){
+
+            int pick = mask & -mask;
+            mask -= pick;
+
+            int d = __builtin_ctz(pick);
+
+            board[r][c] = '1' + d;
+
+            row[r] |= pick;
+            col[c] |= pick;
+            box[b] |= pick;
+
+            if(solve(board, idx+1)) return true;
+
+            row[r] ^= pick;
+            col[c] ^= pick;
+            box[b] ^= pick;
+
+            board[r][c] = '.';
+        }
+
+        return false;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+
+        for(int r=0;r<9;r++){
+            for(int c=0;c<9;c++){
+
+                if(board[r][c]=='.'){
+                    empty.push_back({r,c});
+                }
+                else{
+                    int d = board[r][c]-'1';
+                    int b = (r/3)*3 + c/3;
+
+                    int bit = 1<<d;
+
+                    row[r] |= bit;
+                    col[c] |= bit;
+                    box[b] |= bit;
                 }
             }
         }
-        return true; 
-    }
-    void solveSudoku(vector<vector<char>>& board) {
-        for(int r = 0; r < 9; r++){
-            for(int c = 0; c < 9; c++){
-                if(board[r][c] != '.'){
-                    int d = board[r][c] - '0';
-                    int b = (r/3)*3 + (c/3);
-                    row[r][d] = col[c][d] = box[b][d] = true;
-                 }
-            }
-        }
-        solve(board);
+
+        solve(board,0);
     }
 };
