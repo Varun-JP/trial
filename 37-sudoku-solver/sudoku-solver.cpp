a1f9aa2cd91eacf18f0fcc52bc6877546965fb0f
Varun JP <92174003+Varun-JP@users.noreply.github.com>
2026-03-09 07:55:38 +0530

    Time: 78 ms (93.45%) | Memory: 8.9 MB (36.27%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool row[9][10];
+    bool col[9][10];
+    bool box[9][10];
+    bool solve(vector<vector<char>> &board ){
+        for(int r = 0; r< 9 ; r++){
+            for(int c =0 ; c < 9 ; c++){
+                if(board[r][c] == '.'){
+                    int b = (r/3)*3 + (c/3);
+                    for(int d =1 ; d<= 9 ;d++){
+                        if(row[r][d] || col[c][d] || box[b][d])   continue;
+                        board[r][c] = '0' + d ;
+                        row[r][d] = 1; 
+                        col[c][d] = 1;
+                        box[b][d] =1;
+                        if(solve(board)) return true;
+                        row[r][d] = 0; 
+                        col[c][d] = 0;
+                        box[b][d] =0;
+                        board[r][c] = '.' ;            
+                    }
+                    return false;
+                }
+            }
+        }
+        return true; 
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        for(int r = 0; r < 9; r++){
+            for(int c = 0; c < 9; c++){
+                if(board[r][c] != '.'){
+                    int d = board[r][c] - '0';
+                    int b = (r/3)*3 + (c/3);
+                    row[r][d] = col[c][d] = box[b][d] = true;
+                 }
+            }
+        }
+        solve(board);
+    }
+};
