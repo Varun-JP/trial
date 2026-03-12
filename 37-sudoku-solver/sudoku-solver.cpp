9907d8ee99270a171532cc7fffe91afee9dfe9e4
Varun JP <92174003+Varun-JP@users.noreply.github.com>
2026-03-13 01:10:54 +0530

    Time: 5 ms (98.41%) | Memory: 8.9 MB (10.12%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -6,7 +6,7 @@ public:
     bool dfs(vector<vector<char>>& board, int idx){
         if(idx == empty.size()) return true;
 
-        int best = idx;
+        int best = 0;
         int minOptions = 10;
 
         for(int i = idx; i < empty.size(); i++){
@@ -52,11 +52,12 @@ public:
         row.assign(9, vector<bool>(10,false));
         col.assign(9, vector<bool>(10,false));
         box.assign(9, vector<bool>(10,false));
+//creates [false,false,false,false,false,false,false,false,false,false] 
 
         for(int r = 0; r < 9; r++){
             for(int c = 0; c < 9; c++){
                 if(board[r][c] == '.')
-                    empty.push_back({r,c});
+                    empty.push_back({r,c}); //empty = [(0,2),(0,3),(1,1),(1,2)]
                 else{
                     int d = board[r][c] - '0';
                     int b = (r/3)*3 + c/3;
@@ -64,7 +65,49 @@ public:
                 }
             }
         }
-
+//this entire thing is basically preprocessing for faster lookups from the function
         dfs(board,0);
     }
 };
+
+//back tracking but no dfs
+/*class Solution {
+public:
+    bool row[9][10]; 
+    // since d is from 1-9 we have to have 10 available rows for that  aceess
+    bool col[9][10]; 
+    bool box[9][10];
+    bool solve(vector<vector<char>> &board ){
+        for(int r = 0; r< 9 ; r++){
+            for(int c =0 ; c < 9 ; c++){
+                if(board[r][c] == '.'){
+                    int b = (r/3)*3 + (c/3); //index = row * width + col
+                    for(int d =1 ; d<= 9 ;d++){
+                        if(row[r][d] || col[c][d] || box[b][d])   continue;
+                        board[r][c] = '0' + d ; 
+                        // row[r][d] → true if digit d is used in row r.
+                        row[r][d] = 1, col[c][d] = 1 , box[b][d] =1;
+                        if(solve(board)) return true; //since sudoku only has 1 path
+                        row[r][d] = 0, col[c][d] = 0 , box[b][d] =0;
+                        board[r][c] = '.' ;            
+                    }
+                    return false; //all digits failed for the curr return , backtrack
+                }
+            }
+        }
+        return true; //no empty cell left , board is filled
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        for(int r = 0; r < 9; r++){
+            for(int c = 0; c < 9; c++){
+                if(board[r][c] != '.'){
+                    int d = board[r][c] - '0';
+                    int b = (r/3)*3 + (c/3);
+                    row[r][d] = col[c][d] = box[b][d] = 1;
+                 }
+            }
+        }
+        solve(board);
+    }
+};
+*/
